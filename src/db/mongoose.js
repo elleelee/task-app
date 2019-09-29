49fad8d1b543a3e737d1c2dc2aacf87fd70f1492
Ellen Lee <ellen811003@gmail.com>
2019-09-30 00:59:53 +0800

    set up mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,68 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useUnifiedTopology: true
 })
 
+const User = mongoose.model('User', {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if(!validator.isEmail(value)) {
+          throw new Error('Email is invalid')
+        }
+      }
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if(value < 0) {
+          throw new Error('age must be a positive number')
+        }
+      }
+    }
+})
+
+const me = new User({
+  name: '  Andrew   ',
+  email: 'MYEMAIL@MEAD.IO'
+})
+
+me.save().then(() => {
+  console.log(me)
+}).catch((error)=> {
+  console.log('Error!',error)
+})
+
+const Task = mongoose.model('Task', {
+    description: {
+      type: String
+    },
+    completed: {
+      type: Boolean
+    }
+})
+
+// const taskOne = new Task({
+//   description: 'Doing laundry',
+//   completed: false
+// })
+
+// taskOne.save().then(() => {
+//   console.log(taskOne)
+// }).catch((error)=> {
+//   console.log('Error!',error)
+// })
