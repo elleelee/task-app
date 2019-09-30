20db3750d3effe11a5bfbc75903948b77c1f7bca
Ellen Lee <ellen811003@gmail.com>
2019-09-30 15:07:19 +0800

    added login router

--- src/index.js
+++ src/index.js
@@ -17,15 +17,16 @@ app.listen(port, () => {
 const bcrypt = require('bcryptjs')
 
 const myFunction = async () => {
-  const password = 'Red12345!'
-  const hashedPassword = await bcrypt.hash(password, 8)
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
 
-  console.log(password)
-  console.log(hashedPassword)
+    console.log(password)
+    console.log(hashedPassword)
 
-  const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
-  console.log(isMatch)
+    const isMatch = await bcrypt.compare('red12345!', hashedPassword)
+    console.log(isMatch)
 }
 
 myFunction()
+
 
