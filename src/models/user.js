6635c0fa9e44f73910fbd13b7fee6c0237f18728
Ellen Lee <ellen811003@gmail.com>
2019-09-30 18:53:00 +0800

    completed user/task relationship & cascade delete tasks

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 
 const userSchema = new mongoose.Schema({
@@ -50,6 +51,12 @@ const userSchema = new mongoose.Schema({
      }]
  })
 
+userSchema.virtual('tasks', {
+  ref: 'Task',
+  localField: '_id',
+  foreignField: 'owner'
+})
+
 // Instance method
 userSchema.methods.toJSON = function () {
   const user = this
@@ -97,6 +104,13 @@ userSchema.pre('save', async function (next) {
     }
 
     next()
+})
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+  const user = this
+  await Task.deleteMany({ owner: user._id })
+  next()
 })
 
 
